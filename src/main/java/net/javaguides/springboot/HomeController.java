3f9bbf4e7f0ef67a37667700fb9a26cc6342b773
ShumiPL <some.player.acc@gmail.com>
2024-01-02 13:25:46 +0100

    initial commit

---
+++ src/main/java/net/javaguides/springboot/HomeController.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping({"/","home","/status"})
+	public String getStatus()
+	{
+		return "Application is up and running";
+	}
+}
