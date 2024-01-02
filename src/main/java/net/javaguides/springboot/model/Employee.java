569095a14b6997bbf189b9c92d9c43ac343a971d
ShumiPL <some.player.acc@gmail.com>
2024-01-02 15:06:53 +0100

    configure datasource again

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,61 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_employees")
+public class Employee 
+{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "name")
+	private String firstName;
+	@Column(name = "surname")
+	private String lastName;
+	//@Column(name = "email_address")
+	//private String emailId;
+	
+	
+	public Employee()
+	{
+		super();
+	}
+	
+	public Employee(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		//this.emailId = email;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/*
+	 * public String getEmailId() { return emailId; } public void setEmailId(String
+	 * emailId) { this.emailId = emailId; }
+	 */
+
+	
+}
